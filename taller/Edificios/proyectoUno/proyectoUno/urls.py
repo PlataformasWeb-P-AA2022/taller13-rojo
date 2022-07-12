0db07f3c732b2ecc142d88c8ac5e235c034a22af
RommelOjeda <rommelojeda7@gmail.com>
2022-07-12 15:20:08 -0500

    avance

--- taller/Edificios/proyectoUno/proyectoUno/urls.py
+++ taller/Edificios/proyectoUno/proyectoUno/urls.py
@@ -15,9 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 # from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+
+from rest_framework import routers
+from administrativo import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'edificios', views.EdificioSerializer)
+router.register(r'departamentos', views.DepartamentoSerializer)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('administrativo.urls')),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
